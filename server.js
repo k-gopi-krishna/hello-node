72ab6cacfe6d3f08c2e73c701ac77979745743d5
k-gopi-krishna <119429981+k-gopi-krishna@users.noreply.github.com>
2024-07-27 21:48:11 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express();
+const {PORT=3000}=process.env
+
+app.get('/',(req ,res)=>{
+    res.send( "hello world")
+})
+app.listen(PORT,()=>{
+    console.log(`example app listening on port ${PORT}`)
+}
+
+)
