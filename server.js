a0b994e91ce3dab8b9a810928cb834e2ab17322c
k-gopi-krishna <119429981+k-gopi-krishna@users.noreply.github.com>
2024-07-27 22:11:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=express();
 const {PORT=3000}=process.env
 
 app.get('/',(req ,res)=>{
-    res.send( "hello world")
+    res.send( "!hello world")
 })
 app.listen(PORT,()=>{
     console.log(`example app listening on port ${PORT}`)
